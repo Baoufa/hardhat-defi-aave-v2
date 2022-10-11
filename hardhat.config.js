aefbe75d95dd76f0d4382ac36f532b06def5c7b5
Ben A <benjamin.anoufa@gmail.com>
2022-10-11 17:32:10 +0200

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,67 @@
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+const PRIVATE_KEY = process.env.DEV_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const CMC_API_KEY = process.env.CMC_API_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: {
+        compilers: [
+            { version: "0.8.17" },
+            { version: "0.6.12" },
+            { version: "0.6.6" },
+            { version: "0.4.19" },
+        ],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "EUR",
+        // coinmarketcap: CMC_API_KEY,
+        // token: 'MATIC',
+    },
+    mocha: {
+        timeout: 300000, // 200 sec max
+    },
+};
